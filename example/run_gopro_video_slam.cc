8274801c243d6d990eec6a8ced920bf2d9bef0cc
Steffen Urban <urbste@googlemail.com>
2020-04-03 14:39:25 +0200

    added gopro example

--- example/run_gopro_video_slam.cc
+++ example/run_gopro_video_slam.cc
@@ -13,6 +13,7 @@
 #include <chrono>
 #include <numeric>
 
+#include <opencv2/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/videoio.hpp>
@@ -65,16 +66,30 @@ void mono_tracking(const std::shared_ptr<openvslam::config>& cfg,
     double timestamp = 0.0;
 
     unsigned int num_frame = 0;
-
+    unsigned int empty_frame = 0;
     bool is_not_end = true;
     // run the SLAM in another thread
     std::thread thread([&]() {
         while (is_not_end) {
             is_not_end = video.read(frame);
-
+            // this is necessary as sometimes frames are missing from e.g. gopro videos
+            // and the feed would crash
+            if (!is_not_end) {
+                ++empty_frame;
+                is_not_end = true;
+                if (empty_frame > 500) {
+                    is_not_end = false;
+                }
+                spdlog::debug("VIDEO: empty image nr "+std::to_string(empty_frame)+" in video.");
+            }
             const auto tp_1 = std::chrono::steady_clock::now();
 
             if (!frame.empty() && (num_frame % frame_skip == 0)) {
+                // downsample images from original video to calibrated values
+                // otherwise tracking on orginal FullHD or even bigger GoPro videos is way to slow
+                if (cfg->camera_->cols_ != frame.cols) {
+                    cv::resize(frame, frame, cv::Size(cfg->camera_->cols_, cfg->camera_->rows_));
+                }
                 // input the current frame and estimate the camera pose
                 SLAM.feed_monocular_frame(frame, timestamp, mask);
             }
